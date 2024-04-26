ddb0669277d00db8b4a27f26d27c8bbbd6bb5d58
Aachman Mittal <m.aachman@gmail.com>
2024-04-26 13:04:58 +0530

    Add Book constructor and add new book form

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log("Hello World");
+const myLibrary = [];
+
+function Book(title, author, pageCount, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.isRead = isRead;
+
+  this.info = function () {
+    return `${title} by ${author}, ${pageCount}, ${
+      isRead ? "have read" : "not read yet"
+    }.`;
+  };
+}
+
+function addBookToLibrary({ title, author, pageCount, isRead }) {
+  const newBook = new Book(title, author, pageCount, Boolean(+isRead));
+  myLibrary.push(newBook);
+}
+
+const dialog = document.querySelector(".add-book-dialog");
+const showButton = document.querySelector(".new-book-btn");
+const closeButton = document.querySelector("#submit-btn");
+const bookForm = document.querySelector("#new-book-form");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+bookForm.addEventListener("submit", (event) => {
+  const formData = new FormData(event.target);
+  let data = formData.entries().reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+
+  console.log(data);
+  addBookToLibrary(data);
+
+  // refreshBookDOM();
+  bookForm.reset();
+});
